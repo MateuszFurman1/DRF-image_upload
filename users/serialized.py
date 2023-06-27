cfc525293f70409f31edb7ff822161581d196b5c
MateuszFurman1 <106817902+MateuszFurman1@users.noreply.github.com>
2023-06-27 22:11:30 +0200

    reset_password view added

--- users/serialized.py
+++ users/serialized.py
@@ -1,7 +1,15 @@
+from base64 import urlsafe_b64decode
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from users.models import CustomUser
+from Core import settings
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import send_mail
+from django.utils.encoding import smart_str
+
+
+User = settings.AUTH_USER_MODEL
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -17,11 +25,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         return token
 
 
-class RegisterSerializer(ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     password_confirmation = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = (
             'first_name', 'email', 'password', 'password_confirmation'
         )
@@ -35,7 +43,7 @@ class RegisterSerializer(ModelSerializer):
         return data
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        user = User.objects.create(
             first_name=validated_data.get('first_name'),
             email=validated_data.get('email'),
         )
@@ -46,9 +54,16 @@ class RegisterSerializer(ModelSerializer):
         return user
     
     
-class UserChangePasswordSerializer(serializers.Serializer):
-    password_confirmation = serializers.CharField(write_only=True,
-                                                style={'input_type':'password'}, required=True)
+class ProfileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ('first_name', 'email')
+    
+    
+class ChangePasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    password_confirmation = serializers.CharField(write_only=True, style={'input_type':'password'}, required=True)
+    
     class Meta:
         fields = ['password', 'password_confirmation']
     
@@ -58,4 +73,56 @@ class UserChangePasswordSerializer(serializers.Serializer):
             raise serializers.ValidationError('Passwords do not match')
         user.set_password(data.get('password'))
         user.save()
+        return data
+    
+
+class SendPasswordResetEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+
+    def validate(self, data):
+        email = data.get('email')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('You are not a registered user.')
+
+        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        token = PasswordResetTokenGenerator().make_token(user)
+        reset_link = f'http://localhost:3000/api/user/reset/{uid}/{token}'
+
+        # Send email
+        subject = 'Reset Your Password'
+        body = f'Click the following link to reset your password: {reset_link}'
+        to_email = user.email
+
+        send_mail(subject, body, 'sender@example.com', [to_email], fail_silently=False)
+
+        return data
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
+    password_confirmation = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
+
+    def validate(self, data):
+        password = data.get('password')
+        password_confirmation = data.get('password_confirmation')
+        uid = self.context.get('uid')
+        token = self.context.get('token')
+
+        if password != password_confirmation:
+            raise serializers.ValidationError("Password and Confirm Password don't match")
+
+        try:
+            user_id = smart_str(urlsafe_b64decode(uid))
+            user = User.objects.get(id=user_id)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            raise serializers.ValidationError('Invalid user ID')
+
+        if not PasswordResetTokenGenerator().check_token(user, token):
+            raise serializers.ValidationError('Token is not valid or expired')
+
+        user.set_password(password)
+        user.save()
+
         return data
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from Core import settings
-from users.serialized import MyTokenObtainPairSerializer, RegisterSerializer, UserProfileSerializer
+from users import serialized
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -11,12 +11,14 @@ from rest_framework.views import APIView
 User = settings.AUTH_USER_MODEL
 
 class LoginView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+    serializer_class = serialized.MyTokenObtainPairSerializer
+    permission_classes = [AllowAny]
+
 
 
 class RegisterView(CreateAPIView):
-    serializer_class = RegisterSerializer
-    permission_classes = (AllowAny, )
+    serializer_class = serialized.RegisterSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -25,7 +27,7 @@ class RegisterView(CreateAPIView):
         user = serializer.create(
             validated_data=serializer.validated_data
         )
-        refresh = MyTokenObtainPairSerializer.get_token(user)
+        refresh = serialized.MyTokenObtainPairSerializer.get_token(user)
 
         return Response({
             'refresh': str(refresh),
@@ -33,9 +35,49 @@ class RegisterView(CreateAPIView):
         }, status=status.HTTP_201_CREATED)
         
         
-class UserProfileView(APIView):
+class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
   
     def get(self, request, format=None):
-        serializer = UserProfileSerializer(request.user)
+        serializer = serialized.ProfileSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = serialized.ChangePasswordSerializer(
+            data=request.data,
+            context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+    
+        return Response({'detail': 'Password changed successfully'},
+                    status=status.HTTP_200_OK)
+        
+
+class SendPasswordResetEmailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = serialized.SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.send_reset_email()
+
+        return Response({'detail': 'Password reset link sent. Please check your email.'},
+            status=status.HTTP_200_OK)
+        
+        
+class PasswordResetView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, uid, token, format=None):
+        serializer = serialized.UserPasswordResetSerializer(
+            data=request.data,
+            context={'uid': uid, 'token': token})
+        serializer.is_valid(raise_exception=True)
+        serializer.reset_password()
+
+        return Response({'detail': 'Password reset successfully'},
+            status=status.HTTP_200_OK)
